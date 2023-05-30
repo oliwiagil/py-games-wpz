8512bd2eb889c299a4c47d5d063ea8341a934729
lenamiz <49653259+lenamiz@users.noreply.github.com>
2023-05-30 23:38:02 +0200

    add constant game values

---
+++ crossout/const.py
@@ -0,0 +1,16 @@
+ROW_COUNT = 6
+COLUMN_COUNT = 7
+BLUE = (0, 0, 255)
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+GREEN = (31, 122, 31)
+PINK = (204, 0, 153)
+
+SQUARESIZE = 100
+width = COLUMN_COUNT * SQUARESIZE
+height = (ROW_COUNT+1) * SQUARESIZE
+
+size = (width, height)
+
+RADIUS = int(SQUARESIZE/2 - 5)
+
